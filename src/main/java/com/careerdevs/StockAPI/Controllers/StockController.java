3f9a979296b64c09354a28061c5be3d2f7bb5a6c
Kat Camp <kcamp552369@gmail.com>
2022-03-15 18:34:35 -0400

    initial setup

--- src/main/java/com/careerdevs/StockAPI/Controllers/StockController.java
+++ src/main/java/com/careerdevs/StockAPI/Controllers/StockController.java
@@ -3,10 +3,30 @@ package com.careerdevs.StockAPI.Controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/stocks")
 
 
 public class StockController {
+
+
+    private static String myAlphaKey = "demo";
+    private static String stockInfoEndpoint = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo + myAlphaKey " ;
+
+    @GetMapping("/overview")
+
+
+
+    public static Object getStockHandler (RestTemplate restTemplate) {
+        return restTemplate.getForObject(stockInfoEndpoint, Object.class);
+    }
+
+    public static String getStockInfoEndpoint() {
+        return  stockInfoEndpoint;
+    }
+
 }
+
+
