a76fb96e321622da8483d03909e0169e99918a12
Kat Camp <kcamp552369@gmail.com>
2022-03-15 18:02:44 -0400

    initial setup

---
+++ src/main/java/com/careerdevs/StockAPI/Controllers/StockController.java
@@ -0,0 +1,12 @@
+package com.careerdevs.StockAPI.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stocks")
+
+
+public class StockController {
+}
